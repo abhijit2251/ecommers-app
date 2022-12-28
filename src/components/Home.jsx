f5c35e85ea263d3ae47c51c61b0a764907bcda4d
abhijit2251 <abhijit2251@gmail.com>
2022-12-28 19:21:59 +0530

    added Feature component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./Hero";
 import MainItem from "./MainItem";
+import FeaturedSection from "./FeaturedSection";
 import "./Home.css";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="Home">
       <Hero />
       <MainItem />
+      <FeaturedSection />
     </div>
   );
 };
