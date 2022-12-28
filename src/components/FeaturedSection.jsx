a3fdf4451e3ab38c7ba5bfa63e0e9b7049ae1128
abhijit2251 <abhijit2251@gmail.com>
2022-12-28 19:53:42 +0530

    created featured product list

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Product from "./Product";
+import { ProductsContext } from "../contexts/ProductsContext";
 import "./FeaturedSection.css";
 
 const FeaturedSection = () => {
-  return <div>FeaturedSection</div>;
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="FeaturedSection">
+      <h1>Featured Collection</h1>
+      <div className="products-container">
+        {products?.map((product) => {
+          return product.id <= 4 ? (
+            <Product product={product} key={product.id} />
+          ) : (
+            ""
+          );
+        })}
+      </div>
+      <Link>
+        <button className="view-products-btn">View All Products</button>
+      </Link>
+    </div>
+  );
 };
 
 export default FeaturedSection;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Product.css";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  return (
+    <div className="Product">
+      <div className="product-img">
+        <img src={product.imgURL} alt="product img" />
+      </div>
+      <h2>{product.title}</h2>
+      <p>${product.price}</p>
+    </div>
+  );
 };
 
 export default Product;
